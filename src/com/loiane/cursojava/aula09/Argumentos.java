258e736079c26cc0d9c5d151c816f67334142a8c
Damyres <damyresmaciell@gmail.com>
2023-06-22 14:37:12 -0300

    Aula 09 - Exemplos

---
+++ src/com/loiane/cursojava/aula09/Argumentos.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula09;
+
+public class Argumentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+
+
+}
---
+++ src/com/loiane/cursojava/aula09/Argumentos2.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula09;
+
+public class Argumentos2 {
+    public static void main(String[] args) {
+
+        //Praticando erros
+
+//        System.out.println("Oi")
+    }
+}
