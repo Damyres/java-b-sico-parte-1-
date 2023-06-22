2e2e21941e8d5805ac2960295d90d74d918784d1
Damyres <damyresmaciell@gmail.com>
2023-06-22 14:51:31 -0300

    Aula 13 - Exemplos

---
+++ src/com/loiane/cursojava/aula13/OperadoresAritmeticos.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula13;
+
+import java.util.Scanner;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        Scanner scan = new  Scanner(System.in);
+
+        int num = 3;
+        //num ++;
+        ++num;
+        System.out.println(num);
+
+
+
+
+    }
+}
---
+++ src/com/loiane/cursojava/aula13/OperadoresRealacionais.java
@@ -0,0 +1,8 @@
+package com.loiane.cursojava.aula13;
+
+public class OperadoresRealacionais {
+
+    public static void main(String[] args) {
+        // &&,  ||, !
+    }
+}
