584719651f68acd36b49a1b7e067048f6c4e8450
Damyres <damyresmaciell@gmail.com>
2023-07-03 14:54:43 -0300

    Adicionando aula 30

---
+++ src/com/loiane/cursojava/aula30/Carro.java
@@ -0,0 +1,53 @@
+package com.loiane.cursojava.aula30;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    public Carro(){
+
+    }
+
+    public Carro(String marca, String modelo, int numPassageiros) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        System.out.println("Com 3 parametros");
+    }
+
+    public Carro(String marca, String modelo) {
+        this("Fiat", "Ducato", 10);
+        System.out.println("Com 2 parametros");
+    }
+
+    public Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consumoCombustivel = consumoCombustivel;
+
+    }
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + (capCombustivel * consumoCombustivel));
+    }
+
+    double obterAutonomia() {
+        return (capCombustivel * consumoCombustivel);
+    }
+    double calcularCombustivel(double km){
+        double qtdCombustivel = km/consumoCombustivel;
+        return qtdCombustivel;
+    }
+
+
+    public String toString(){
+        return "Marca: " + this.marca + "\n" + "Modelo: " + this.modelo + "\n"
+                + "Número de passageiros: " + this.numPassageiros + "\n"
+                + "Capacidade de combustível: " + this.capCombustivel + "\n"
+                + "Consumo de combustível: " + this.consumoCombustivel;
+    }
+}
---
+++ src/com/loiane/cursojava/aula30/PrincipalCarro.java
@@ -0,0 +1,15 @@
+package com.loiane.cursojava.aula30;
+
+public class PrincipalCarro {
+    public static void main(String[] args) {
+
+        Carro van = new Carro("Fiat", "Ducato");
+
+        Carro kombi = new Carro("Chevrolet", "Kombi", 10);
+
+        System.out.println(kombi.toString());
+
+
+
+    }
+}
