5e79814b45f5519ad7d7f53abad1073ef4dc2ab0
Damyres <damyresmaciell@gmail.com>
2023-07-06 20:49:11 -0300

    Adicionando aula 33

---
+++ src/com/loiane/cursojava/aula33/Exercico.java
@@ -0,0 +1,17 @@
+package com.loiane.cursojava.aula33;
+
+public class Exercico {
+    public static void main(String[] args) {
+
+        MinhaCalculadora cal = new MinhaCalculadora();
+//        cal.soma(1,2);
+        System.out.println(cal.soma(1,2));
+
+
+        System.out.println( cal.soma(2.0,5.6));
+
+    }
+
+
+
+}
