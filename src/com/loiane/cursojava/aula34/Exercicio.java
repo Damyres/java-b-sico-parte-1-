842e0c95df8168ea9cd90d056c9e158b17a0bab0
Damyres <damyresmaciell@gmail.com>
2023-07-07 17:19:10 -0300

    Adicionando aula34

---
+++ src/com/loiane/cursojava/aula34/Exercicio.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula34;
+
+public class Exercicio {
+
+    static int resultSoma;
+    public static void main(String[] args) {
+
+        System.out.println(MinhaCalculadora.soma(1,5));
+
+
+
+
+    }
+}
